3dcf303f6f2c1b459f518500c080994b47f67185
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-11-27 21:48:49 +0900

    #2 async, fetch

--- src/main/java/com/example/thymeleaf/controller/MainController.java
+++ src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -2,8 +2,7 @@ package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +12,28 @@ public class MainController {
     public String index(Model model){
          model.addAttribute("str", "Hello Spring Boot JPA Programming");
         return "index"; // index.html -> ${str}
+    }
+
+    @PostMapping("/submitData")
+    @ResponseBody
+    public String submitData(@RequestBody String data) {
+        System.out.println(data);
+        return "Data successfully submitted";
+    }
+
+    @GetMapping("/fetchTest")
+    public String fetchTest() {
+        //return "fetchTest";
+        return "fetchTest2";
+    }
+
+    @GetMapping("/jsonPlace")
+    public String jsonPlace() {
+        return "jsonplace";
+    }
+
+    @GetMapping("/doc")
+    public String doc() {
+        return "doc";
     }
 }
